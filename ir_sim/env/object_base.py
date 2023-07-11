581424be994ced8c6cd99496cec8c78b17e7f3cd
Han <744475118@qq.com>
2023-07-11 21:51:34 +0800

    prepare for shapely

---
+++ ir_sim/env/object_base.py
@@ -0,0 +1,40 @@
+
+from typing import Any
+
+import numpy as np
+from shapely import 
+
+
+
+class object_base:
+    def __init__(self, ) -> None:
+
+        '''
+        basic properties of an object:
+
+            id: 
+
+            geometry:
+                shape:
+
+            state:
+                pass
+
+            center: geometry center
+            orientation: angle with x axis
+
+            velocity:
+
+            dynamic:
+                pass
+
+            
+        '''
+
+
+
+
+        self.geometry = None
+        pass
+        
+        
