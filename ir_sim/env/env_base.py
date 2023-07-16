dc6fe8902caa182a2d14efd4fe007baf881ac64c
Herry_robot <744475118@qq.com>
2023-07-16 23:16:31 +0800

    Update env_base.py

--- ir_sim/env/env_base.py
+++ ir_sim/env/env_base.py
@@ -1,7 +1,9 @@
 import yaml
 
 from ir_sim.util.util import file_check
-from ir_sim.world import world
+from ir_sim.world import world, MultiRobots
+from env_plot import EnvPlot
+
 
 class EnvBase:
 
@@ -19,23 +21,26 @@ class EnvBase:
 
         world_file_path = file_check(world_name)
         
+        world_kwargs, plot_kwargs = dict(), dict()
+
+
         if world_file_path != None:
            
             with open(world_file_path) as file:
                 com_list = yaml.load(file, Loader=yaml.FullLoader)
-                world_args = com_list.get('world', dict())
+                world_kwargs = com_list.get('world', dict())
 
-        world_args |= kwargs.get('world_args', dict())
-
-
+        world_kwargs |= kwargs.get('world', dict())
+        plot_kwargs |= kwargs.get('plot', dict())
+        
 
         # init world, robot, obstacles
-        self.world = world(**world_args)
-    
-    
-
-    
-    
+        self.world = world(**world_kwargs)
+        self.robots = MultiRobots()
+        self.obstacles = MultiRobots()
+        # self.objects = self.robots.objects + self.obstacles.objects
+        
+        self.env_plot = EnvPlot(grid_map=self.world.grid_map, objects=self.objects, **plot_kwargs)
 
 
 
